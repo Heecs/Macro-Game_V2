5df0b7d1613920ddc226f53149a7b9ab428ae8b8
NeranFen <kuehneflorian-mail@web.de>
2026-05-15 09:49:14 +0100

    added light slider

--- Assets/Scripts/FilmmakingManager.cs
+++ Assets/Scripts/FilmmakingManager.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.Splines;
 
 public class FilmmakingManager : MonoBehaviour
 {
     public Camera cam;
+    public Light lightbulb;
+    public int lightBaseValue = 100;
 
     private Vector3 camAnchor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,6 +25,11 @@ public class FilmmakingManager : MonoBehaviour
     {
         print(angle);
         cam.transform.rotation = Quaternion.Euler(angle,0,0);
-        cam.transform.position = camAnchor + new Vector3(0, (angle/55)*5, (angle / 55) * 5);
+        cam.transform.position = camAnchor + new Vector3(0, (angle/55)*10, 0);
+    }
+
+    public void ChangeLighting(float intensity)
+    {
+        lightbulb.intensity = intensity + lightBaseValue;
     }
 }
