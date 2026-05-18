e8affe0c6ceae085523d1d637ccde483e11b57f2
NeranFen <44315589+NeranFen@users.noreply.github.com>
2026-05-18 14:14:34 +0200

    Finished Blockout (missing Game Logic)

--- Assets/Scripts/FilmmakingManager.cs
+++ Assets/Scripts/FilmmakingManager.cs
@@ -1,27 +1,46 @@
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
-using UnityEngine.Splines;
 using UnityEngine.UI;
 
 public class FilmmakingManager : MonoBehaviour
 {
+    
+    [Header("Camera Settings")]
     public Camera cam;
-    public Light lightbulb;
     public Slider camSlider;
-    public int lightBaseValue = 100;
+    public int camRangeY;
+    public int camRangeZ;
+    public int minCamAngle;
+    public int maxCamAngle;
+    public int startingAngleOffset;
 
-    public int rangeY;
-    public int rangeZ;
-    public int minAngle;
-    public int maxAngle;
+    [Header("Light Settings")]
+    public Light lightbulb;
+    public Slider lightSlider;
+    public int minIntensity;
+    public int maxIntensity;
+    public int startingIntensityOffset;
+
+    [Header("Sound Settings")]
+    public AudioSource audioSource;
+    public Slider audioSlider;
+    public float startingVolume;
+
 
     private Vector3 camAnchor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         camAnchor = cam.transform.position;
-        camSlider.minValue = minAngle;
-        camSlider.maxValue = maxAngle;
+        camSlider.minValue = minCamAngle;
+        camSlider.maxValue = maxCamAngle;
+        lightSlider.minValue = minIntensity;
+        lightSlider.maxValue = maxIntensity;
+        RotateCamera(startingAngleOffset);
+        camSlider.value = startingAngleOffset;
+        ChangeLighting(startingIntensityOffset);
+        lightSlider.value = startingIntensityOffset;
+        AdjustVolume(startingVolume);
+        audioSlider.value = startingVolume;
     }
 
     // Update is called once per frame
@@ -32,13 +51,18 @@ public class FilmmakingManager : MonoBehaviour
 
     public void RotateCamera(float  angle)
     {
-        float range = maxAngle - minAngle;
+        float range = maxCamAngle - minCamAngle;
         cam.transform.rotation = Quaternion.Euler(angle,0,0);
-        cam.transform.position = camAnchor + new Vector3(0, (angle / range) * rangeY, (angle / range) * rangeZ);
+        cam.transform.position = camAnchor + new Vector3(0, (angle / range) * camRangeY, (angle / range) * camRangeZ);
     }
 
     public void ChangeLighting(float intensity)
     {
-        lightbulb.intensity = intensity + lightBaseValue;
+        lightbulb.intensity = intensity + minIntensity;
+    }
+
+    public void AdjustVolume(float volume)
+    {
+        audioSource.volume = volume;
     }
 }
