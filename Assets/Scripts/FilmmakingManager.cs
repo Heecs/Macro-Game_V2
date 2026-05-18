08b33b84701aa56ee4ef4d02a2f56fd88a398b5f
NeranFen <44315589+NeranFen@users.noreply.github.com>
2026-05-18 13:02:44 +0200

    Refining

--- Assets/Scripts/FilmmakingManager.cs
+++ Assets/Scripts/FilmmakingManager.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.Splines;
+using UnityEngine.UI;
 
 public class FilmmakingManager : MonoBehaviour
 {
     public Camera cam;
     public Light lightbulb;
+    public Slider camSlider;
     public int lightBaseValue = 100;
+
+    public int rangeY;
+    public int rangeZ;
+    public int minAngle;
+    public int maxAngle;
 
     private Vector3 camAnchor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         camAnchor = cam.transform.position;
+        camSlider.minValue = minAngle;
+        camSlider.maxValue = maxAngle;
     }
 
     // Update is called once per frame
@@ -23,9 +32,9 @@ public class FilmmakingManager : MonoBehaviour
 
     public void RotateCamera(float  angle)
     {
-        print(angle);
+        float range = maxAngle - minAngle;
         cam.transform.rotation = Quaternion.Euler(angle,0,0);
-        cam.transform.position = camAnchor + new Vector3(0, (angle/55)*10, 0);
+        cam.transform.position = camAnchor + new Vector3(0, (angle / range) * rangeY, (angle / range) * rangeZ);
     }
 
     public void ChangeLighting(float intensity)
