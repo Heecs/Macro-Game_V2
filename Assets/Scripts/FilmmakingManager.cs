8ed5c24f812d5414c6533761fe654c23d87c195a
NeranFen <kuehneflorian-mail@web.de>
2026-05-19 12:45:11 +0100

    Implemented some Backend

--- Assets/Scripts/FilmmakingManager.cs
+++ Assets/Scripts/FilmmakingManager.cs
@@ -1,9 +1,20 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FilmmakingManager : MonoBehaviour
 {
-    
+    public TextMeshProUGUI camGoalText;
+    public TextMeshProUGUI audioGoalText;
+    public TextMeshProUGUI lightGoalText;
+    public GameObject ResultsPanel;
+    public TextMeshProUGUI camResultText;
+    public TextMeshProUGUI camResultGrade;
+    public TextMeshProUGUI audioResultText;
+    public TextMeshProUGUI audioResultGrade;
+    public TextMeshProUGUI lightResultText;
+    public TextMeshProUGUI lightResultGrade;
+
     [Header("Camera Settings")]
     public Camera cam;
     public Slider camSlider;
@@ -12,12 +23,14 @@ public class FilmmakingManager : MonoBehaviour
     public int minCamAngle;
     public int maxCamAngle;
     public int startingAngleOffset;
+    public float camSpeed;
 
     [Header("Light Settings")]
     public Light lightbulb;
     public Slider lightSlider;
     public int minIntensity;
     public int maxIntensity;
+    public float intensityMult;
     public int startingIntensityOffset;
 
     [Header("Sound Settings")]
@@ -25,11 +38,30 @@ public class FilmmakingManager : MonoBehaviour
     public Slider audioSlider;
     public float startingVolume;
 
+    [Header("Target Settings")]
+    public int targetAngle;
+    public int angleGreatRange;
+    public int angleGoodRange;
+    public int angleOkRange;
+    public int targetBrightness;
+    public int lightGreatRange;
+    public int lightGoodRange;
+    public int lightOkRange;
+    public float targetVolume;
+    public float soundGreatRange;
+    public float soundGoodRange;
+    public float soundOkRange;
+
 
     private Vector3 camAnchor;
+    private string camResult;
+    private string lightResult;
+    private string audioResult;
+    private bool isPaused = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        RandomizeTargets();
         camAnchor = cam.transform.position;
         camSlider.minValue = minCamAngle;
         camSlider.maxValue = maxCamAngle;
@@ -41,6 +73,9 @@ public class FilmmakingManager : MonoBehaviour
         lightSlider.value = startingIntensityOffset;
         AdjustVolume(startingVolume);
         audioSlider.value = startingVolume;
+        UpdateGoals();
+        ResultsPanel.SetActive(false);
+        UnpauseGame();
     }
 
     // Update is called once per frame
@@ -49,20 +84,93 @@ public class FilmmakingManager : MonoBehaviour
         
     }
 
+    public void UpdateGoals()
+    {
+        camGoalText.SetText("Camera Target Angle: " + targetAngle + "°");
+        audioGoalText.SetText("Target Volume: " + (int)(targetVolume*100) + "%");
+        lightGoalText.SetText("Target Brightness: " + targetBrightness + "%");
+    }
+
     public void RotateCamera(float  angle)
     {
         float range = maxCamAngle - minCamAngle;
-        cam.transform.rotation = Quaternion.Euler(angle,0,0);
+        cam.transform.rotation = Quaternion.Euler(angle, 0, 0);
         cam.transform.position = camAnchor + new Vector3(0, (angle / range) * camRangeY, (angle / range) * camRangeZ);
     }
 
     public void ChangeLighting(float intensity)
     {
-        lightbulb.intensity = intensity + minIntensity;
+        lightbulb.intensity = intensity * intensityMult + minIntensity;
     }
 
     public void AdjustVolume(float volume)
     {
         audioSource.volume = volume;
+    }
+
+    public void PressRecord()
+    {
+        print(isPaused);
+        if (!isPaused)
+        {
+            float camAccuracy = Mathf.Abs(targetAngle - cam.transform.rotation.eulerAngles.x);
+            float soundAccuracy = Mathf.Abs(audioSource.volume - targetVolume);
+            float lightAccuracy = Mathf.Abs(lightbulb.intensity / intensityMult - targetBrightness);
+            camResult = CheckResults(camAccuracy, angleGreatRange, angleGoodRange, angleOkRange);
+            audioResult = CheckResults(soundAccuracy, soundGreatRange, soundGoodRange, soundOkRange);
+            lightResult = CheckResults(lightAccuracy, lightGreatRange, lightGoodRange, lightOkRange);
+            PauseGame();
+            ResultsPanel.SetActive(true);
+            camResultText.text = "Camera Angle: " + cam.transform.rotation.eulerAngles.x + "°";
+            camResultGrade.text = camResult;
+            audioResultText.text = "Volume: " + audioSource.volume * 100 + "%";
+            audioResultGrade.text = audioResult;
+            lightResultText.text = "Brightness: " + lightbulb.intensity / intensityMult + "%";
+            lightResultGrade.text = lightResult;
+        }
+    }
+
+    private string CheckResults(float accuracy, float forGreat, float forGood, float forOk)
+    {
+        if (accuracy <= forGreat)
+        {
+            return "Great";
+        }
+        else if (accuracy <= forGood)
+        {
+            return "Good";
+        }
+        else if (accuracy <= forOk)
+        {
+            return "Okay";
+        }
+        else
+        {
+            return "Bad";
+        }
+    }
+
+    private void PauseGame()
+    {
+        camSlider.interactable = false;
+        audioSlider.interactable = false;
+        lightSlider.interactable = false;
+        isPaused = true;
+    }
+
+    private void UnpauseGame()
+    {
+        camSlider.interactable = true;
+        audioSlider.interactable = true;
+        lightSlider.interactable = true;
+        isPaused = false;
+    }
+
+    private void RandomizeTargets()
+    {
+        targetAngle = Random.Range(minCamAngle, maxCamAngle + 1);
+        targetBrightness = Random.Range(0, 101);
+        targetVolume = Random.Range(0f, 1f);
+        print(targetAngle + "\n" + targetBrightness + "\n" + targetVolume + "\n");
     }
 }
